35f1c78dbebcf440147e122f0ea5d1f358c8bbbf
Shrey Valia <svalia@ucsc.edu>
2016-01-29 17:29:42 -0800

    firebase integration

--- app/src/main/java/com/example/shreyvalia/evently/MainActivity.java
+++ app/src/main/java/com/example/shreyvalia/evently/MainActivity.java
@@ -2,28 +2,37 @@ package com.example.shreyvalia.evently;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button butt;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Evennntly");
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        butt = (Button) findViewById(R.id.button);
+        butt.setOnClickListener(new MyClass());
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make()
+                Toast.makeText(getApplicationContext(), "Replace with your own action", Toast.LENGTH_LONG).show();
+//                        .setAction("Action", null).show();
             }
         });
     }
@@ -34,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -49,4 +59,14 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public class MyClass implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+    }
+
 }
