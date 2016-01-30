2c1e9b94d30042c9c31e4241a021adc085a6e5fc
Jenna <jennawu0912@gmail.com>
2016-01-30 00:05:56 -0800

    made ma classssssss

---
+++ app/src/main/java/com/example/shreyvalia/evently/node.java
@@ -0,0 +1,27 @@
+package com.example.shreyvalia.evently;
+
+/**
+ * Created by Jenna on 1/30/2016.
+ */
+public class node {
+    String name;
+    String description;
+    // photos (optional)
+    String address;
+    int date;
+    int sTime;
+    int eTime;
+    int nAttend;
+    int max;
+    int rating;
+    String userID;
+    boolean visible;
+
+    public node(String userID) {
+        this.userID = userID;
+        int rating;
+        String name;
+        String description;
+
+    }
+}
