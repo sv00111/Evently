bed6fe73b3fd8978aecc8bd4d8e42a175407269d
kellygit <writetokellyh@gmail.com>
2016-01-30 00:10:18 -0800

    private variables

--- app/src/main/java/com/example/shreyvalia/evently/node.java
+++ app/src/main/java/com/example/shreyvalia/evently/node.java
@@ -8,14 +8,14 @@ public class node {
     String description;
     // photos (optional)
     String address;
-    int date;
-    int sTime;
-    int eTime;
-    int nAttend;
-    int max;
-    int rating;
-    String userID;
-    boolean visible;
+    private int date;
+    private int sTime;
+    private int eTime;
+    private int nAttend;
+    private int max;
+    private int rating;
+    private String userID;
+    private boolean visible;
 
     public node(String userID) {
         this.userID = userID;
