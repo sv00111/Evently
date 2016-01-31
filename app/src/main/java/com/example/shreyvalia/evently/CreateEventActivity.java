2d7b87cf973c4c2d2fd71741d9faac954f29b652
Jenna <jennawu0912@gmail.com>
2016-01-30 20:23:54 -0800

    merge conflicts

--- app/src/main/java/com/example/shreyvalia/evently/CreateEventActivity.java
+++ app/src/main/java/com/example/shreyvalia/evently/CreateEventActivity.java
@@ -1,15 +1,11 @@
 package com.example.shreyvalia.evently;
 
-import android.app.DatePickerDialog;
-import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.EditText;
-
-import java.util.Calendar;
 
 public class CreateEventActivity extends AppCompatActivity {
 
@@ -27,9 +23,9 @@ public class CreateEventActivity extends AppCompatActivity {
     }
 
 
-    public void onStart(){
+    public void onStart() {
         super.onStart();
-        EditText txtDate=(EditText)findViewById(R.id.txtdate_start);
+        EditText txtDate = (EditText) findViewById(R.id.txtdate_start);
         txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -41,22 +37,18 @@ public class CreateEventActivity extends AppCompatActivity {
             }
         });
 
-        txtDate=(EditText)findViewById(R.id.txtdate_end);
-        txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+        txtDate = (EditText) findViewById(R.id.txtdate_end);
+        txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus){
-                if(hasFocus){
-                    DateDialog dialog=new DateDialog((EditText) v.findViewById(R.id.txtdate_end));
-                    FragmentTransaction ft=getFragmentManager().beginTransaction();
-                    dialog.show(ft,"DatePicker");
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    DateDialog dialog = new DateDialog((EditText) v.findViewById(R.id.txtdate_end));
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    dialog.show(ft, "DatePicker");
                 }
             }
         });
     }
 
 
-
-
-
-
-    }
+}
